118f12d82c22546a886970c1b1e0519fc8473cc7
Tanner Eustice <teustice@msn.com>
2017-07-14 09:42:22 -0700

    update seed to include images

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,24 @@ class Seed
   end
 
   def generate_products
+    Product.destroy_all
+    product_images = [
+      "https://codepo8.github.io/canvas-images-and-pixels/img/horse.png",
+      "https://www.dhresource.com/0x0s/f2-albu-g3-M01-45-79-rBVaHVXMYiyAdhjeAAKMXVH87ho921.jpg/horse-boy-mask-halloween-animation-game-show.jpg",
+      "http://www.dhresource.com/260x260s/f2-albu-g5-M00-15-91-rBVaI1iurGWAV019AAFYkCxlCLY176.jpg/1701-full-head-mask-horse-head-mask-creepy.jpg",
+      "http://funnypartyshop.ro/297-home_default/masca-halloween-cal-cu-dunga-alba.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRycYAqVbcgQzxg5rVekIIjGgeMP3-6TBnlyRURCWUv5nAHQ7Dz",
+      "http://www.pngpix.com/wp-content/uploads/2016/03/Horse-PNG-image-1-250x296.png",
+      "http://www.musthavetips.net/wp-content/uploads/2016/10/51Xb6Ad7MfL.jpg"
+    ]
     20.times do |i|
-      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
+      Product.create!(
+        name: Faker::Lorem.word,
+        price: rand(10...100),
+        description: Faker::Lorem.sentence(5, false, 0).chop,
+        image: product_images.sample)
     end
+    p "added #{Product.count} products"
   end
 end
 
